09f635af7c29e1779b9f362fb52d027016908ad2
Achieve-software <vtcbcdtyr@gmail.com>
2023-09-05 23:24:20 +0300

    addmodal

---
+++ src/helper/data.jsx
@@ -0,0 +1,74 @@
+export const appointmentData = [
+  {
+    id: 1,
+    patient: "Barry Vermont",
+    day: new Date(),
+    consulted: true,
+    doctor: "Hazel Valery",
+  },
+  {
+    id: 2,
+    patient: "Harry Coming",
+    day: new Date(),
+    consulted: false,
+    doctor: "Jessica White",
+  },
+  {
+    id: 3,
+    patient: "Aaron Black",
+    day: new Date(),
+    consulted: true,
+    doctor: "Jasmine Carpanter",
+  },
+]
+
+export const doctorData = [
+  {
+    id: 1,
+    name: "Hazel Valery",
+    dep: "Lastik Bakım",
+    img: "./img/dr1.jpg",
+  },
+  {
+    id: 2,
+    name: "Jasmine Carpanter",
+    dep: "Genel Bakım",
+    img: "./img/dr2.jpg",
+  },
+  {
+    id: 3,
+    name: "Arianna Mary",
+    dep: "Kaporta",
+    img: "./img/dr3.jpg",
+  },
+  {
+    id: 4,
+    name: "Jessica White",
+    dep: "Yağ Değişimi",
+    img: "./img/dr4.jpg",
+  },
+  {
+    id: 5,
+    name: "Harry Coming",
+    dep: "Fren Bakım",
+    img: "./img/dr9.jpg",
+  },
+  {
+    id: 6,
+    name: "Kristensen Abraham",
+    dep: "Radyatör Bakım",
+    img: "./img/dr6.jpg",
+  },
+  {
+    id: 7,
+    name: "Asley Graham",
+    dep: "Rot-Balans",
+    img: "./img/dr7.jpg",
+  },
+  {
+    id: 8,
+    name: "Mary Motley",
+    dep: "Egzoz Muayene",
+    img: "./img/dr8.jpg",
+  },
+]
